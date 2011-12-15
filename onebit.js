97747b4c796cb98deb9806eb433f6e56ce0cb1fa
Giannis Dzegoutanis <erasmospunk@gmail.com>
2011-12-15 05:29:33 +0200

    added css to engine

--- onebit.js
+++ onebit.js
@@ -7,9 +7,31 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+app.get('/css/main.css', function (req, res) {
+  res.sendfile(__dirname + '/css/main.css');
+});
+
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
 });
+
+var feed = io.of('/feed')
+  .on('connection', function (socket) {
+    socket.emit('a message', {
+        that: 'only'
+      , '/chat': 'will get'
+    });
+    chat.emit('a message', {
+        everyone: 'in'
+      , '/chat': 'will get'
+    });
+  });
+
+var news = io
+  .of('/news')
+  .on('connection', function (socket) {
+    socket.emit('item', { news: 'item' });
+  });
